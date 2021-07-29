6d6b708be5bdcadb211f16316c07d1283e577aaa
Kinerdo <41324968+Kinerdo@users.noreply.github.com>
2021-07-29 19:11:09 +0100

    api call to quote. text centred

---
+++ inc/main.js
@@ -0,0 +1,13 @@
+const api_url = "https://stoicquotesapi.com/v1/api/quotes/random";
+
+async function getQuote() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  //   console.log(data);
+  const { body, author } = data;
+  console.log(body);
+  console.log(author);
+  document.getElementById("body-quote").textContent = body;
+  document.getElementById("author").textContent = author;
+}
+getQuote();

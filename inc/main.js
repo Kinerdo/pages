396d5e3f7f0d2e115cd4a0dfc6dd2c33dd0eb4c6
Kinerdo <41324968+Kinerdo@users.noreply.github.com>
2021-07-29 19:30:59 +0100

    changed js to fetch

--- inc/main.js
+++ inc/main.js
@@ -1,13 +1,31 @@
-const api_url = "https://stoicquotesapi.com/v1/api/quotes/random";
+// const api_url = "https://stoicquotesapi.com/v1/api/quotes/random";
 
-async function getQuote() {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  //   console.log(data);
-  const { body, author } = data;
-  console.log(body);
-  console.log(author);
-  document.getElementById("body-quote").textContent = body;
-  document.getElementById("author").textContent = author;
-}
-getQuote();
+// async function getQuote() {
+//   const response = await fetch(api_url);
+//   const data = await response.json();
+//   //   console.log(data);
+//   const { body, author } = data;
+//   console.log(body);
+//   console.log(author);
+//   document.getElementById("body-quote").textContent = body;
+//   document.getElementById("author").textContent = author;
+// }
+// getQuote();
+
+fetch("https://stoicquotesapi.com/v1/api/quotes/random")
+  .then(function (response) {
+    // The API call was successful!
+    return response.json();
+  })
+  .then(function (data) {
+    // This is the JSON from our response
+    const { body, author } = data;
+    console.log(body);
+    console.log(author);
+    document.getElementById("body-quote").textContent = body;
+    document.getElementById("author").textContent = author;
+  })
+  .catch(function (err) {
+    // There was an error
+    console.warn("Something went wrong.", err);
+  });
